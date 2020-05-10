783d6f7a9077a54b99d695a9fa823591ae77a13a
saad-pixel <marva.saad2@gmail.com>
2020-05-10 12:34:23 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,25 @@
+struct Person {
+
+var name : String
+var age :  Int
+var city : String
+
+func printHello (){
+  
+print( " hello i am  \(name)  my age is \(age) and i live in \(city) 😉")
+		
+		}
+}
+
+struct waterMeter {
+  var litersUsed : Int = 0
+  var customer : Person = Person(name : "tony", age : 22, city : "ohaio") // initiation
+  
+  }
+  
+  
+let meter = waterMeter()
+
+print (meter.customer)
+print("------------------------------")
+print("meters used is \(meter.litersUsed)")
